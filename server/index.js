3eb20ad7214408de7b4c45ed2e1ad6d57bd8b1ee
Ghulam Akbar <150150845+akbar909@users.noreply.github.com>
2025-05-16 14:42:34 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
 
